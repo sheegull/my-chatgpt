61c2b297b0749dfa9203fd45676dcf8c56a6f6be
sheegull <89774989+sheegulls@users.noreply.github.com>
2023-03-17 11:17:10 +0900

    ✨ create Header section

---
+++ client/src/components/chat/Head.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import bot from "../../assets/bot.svg";
+import { BsCameraVideo, BsTelephone } from "react-icons/bs";
+
+const Head = () => {
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex items-center">
+        <img
+          src={bot}
+          alt="bot-image"
+          className="w-14 h-14 bg-[#176FFF] rounded-2xl justify-center flex items-center p-[2px]"
+        />
+        <div className="text-white ml-4 flex flex-col">
+          <p className="font-bold">ChatBot</p>
+          <p className="text-xs mt-1">Online</p>
+        </div>
+      </div>
+      <div className="flex text-white gap-6 flex-row items-center">
+        <BsCameraVideo className="text-[22px]" />
+        <BsTelephone className="text-[22px]" />
+      </div>
+    </div>
+  );
+};
+
+export default Head;

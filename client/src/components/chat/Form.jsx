429090ec4b3c2bf24e78a4e621b650eda35f713e
sheegull <89774989+sheegulls@users.noreply.github.com>
2023-03-17 23:05:19 +0900

    💄 update form ui

--- client/src/components/chat/Form.jsx
+++ client/src/components/chat/Form.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { format } from "date-fns";
 import axios from "axios";
+import { MdKeyboardVoice } from "react-icons/md";
+import { AiOutlinePaperClip } from "react-icons/ai";
 
 const Form = ({ setMessages }) => {
   const [message, setMessage] = useState("");
@@ -38,15 +40,20 @@ const Form = ({ setMessages }) => {
   };
 
   return (
-    <form className="flex items-center mt-12">
+    <form className="flex items-center mt-12 relative">
       <input
         type="text"
         value={message}
         onChange={(e) => setMessage(e.target.value)}
         placeholder="Type a message"
-        className="bg-[#3A3F47] text-white placeholder:text-[#949494] text-sm rounded-2xl p-4 w-full outline-none"
+        className="relative bg-[#3A3F47] text-white placeholder:text-[#949494] text-sm rounded-full pl-8 p-4 w-full outline-none"
       />
-      <button type="submit" onClick={sendMessage}></button>
+      <button type="submit" onClick={sendMessage}>
+        <AiOutlinePaperClip className="text-[#949494] text-[22px] absolute right-[80px] top-[16px]" />
+      </button>
+      <div className=" bg-[#176FFF] rounded-full p-3 ml-4 cursor-pointer">
+        <MdKeyboardVoice className="text-white text-2xl" />
+      </div>
     </form>
   );
 };
